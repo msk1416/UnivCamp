66742439450e57843ae690315f0fa847a69fc54e
Sergi Mascaró <msk1416@gmail.com>
2018-05-30 12:24:35 +0200

    added database test cases

--- tests/pl/mais/tests/DatabaseTests.java
+++ tests/pl/mais/tests/DatabaseTests.java
@@ -2,11 +2,14 @@ package pl.mais.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.junit.Test;
 
 import pl.mais.db.DBHelper;
+import pl.mais.mapping.Course;
+import pl.mais.mapping.Registration;
 import pl.mais.mapping.User;
 
 public class DatabaseTests {
@@ -35,6 +38,40 @@ public class DatabaseTests {
 		boolean addResult = db.addStudent(u.getFirstName(), u.getLastName(), u.getBirthday(), u.getEmail(), u.getCurrentStudies(), u.getCurrentEcts());
 		assertTrue(addResult);
 		db.close();
+	}
+	
+	@Test
+	public void newInvalidRegistration() {
+		int sid = 600;
+		String courseId = "NEC", status = "cur"; //NEC -> NonExistingCourse
+		double grade = 4.0;
+		db.open();
+		boolean addResult = db.addRegistration(sid, courseId, grade, status);
+		db.close();
+		assertFalse(addResult);
+	}
+	
+	@Test
+	public void newValidRegistration() {
+		int sid;
+		String courseId = null, status = "cur";
+		double grade = 4.5;
+		ArrayList<Registration> regs;
+		ArrayList<Course> courses;
+		db.open();
+		do {
+			courses = db.getCourses();
+			sid = db.getRandomStudentId();
+			regs = db.getRegsFromStudent(sid);
+			for (int i = 0; i < regs.size(); i++) {
+				courses.remove(db.getCourseByShortId(regs.get(i).getCourseId()));
+			}
+			if (courses.size() > 0)
+				courseId = courses.get(0).getShortId();
+		} while (courseId == null);
+		
+		boolean addResult = db.addRegistration(sid, courseId, grade, status);
+		assertTrue(addResult);
 	}
 
 }
